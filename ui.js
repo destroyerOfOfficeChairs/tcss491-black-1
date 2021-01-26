c193fe13366ad2c0d44624fb27f9b4f92d4c44d5
William Bryan <wjbvii@gmail.com>
2021-01-26 17:42:13 -0600

    Now the title screen flashes 'Press B'

--- ui.js
+++ ui.js
@@ -2,6 +2,8 @@ class TitleScreen {
     constructor(game, x, y) {
         Object.assign(this, {game, x, y});
         this.image = ASSET_MANAGER.getAsset("./sprites/title_screen.png");
+        this.spritesheet = ASSET_MANAGER.getAsset("./sprites/pressB.png");
+        this.animation = new Animator(this.spritesheet, 0, 0, 32, 32, 5, 0.1, 0, false, true, true);
     }
 
     update() {
@@ -10,5 +12,6 @@ class TitleScreen {
 
     draw(ctx) {
         ctx.drawImage(this.image, 0, 0, 256, 224);
+        this.animation.drawFrame(this.game.clockTick, ctx, 32, 48, 1);
     }
 }
