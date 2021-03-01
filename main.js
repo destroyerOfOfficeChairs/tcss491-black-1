04523ba653701339895133f60f5e04a51e317323
Vanessa <63383205+laziestsheep@users.noreply.github.com>
2021-02-28 16:13:19 -0800

    Add files via upload
    added code to make music word to each of the files. did not remove any other code

---
+++ main.js
@@ -0,0 +1,81 @@
+var ASSET_MANAGER = new AssetManager();
+
+ASSET_MANAGER.queueDownload("./sprites/DungeonBackground.png");
+ASSET_MANAGER.queueDownload("./sprites/title_screen.png");
+ASSET_MANAGER.queueDownload("./sprites/pressB.png");
+
+ASSET_MANAGER.queueDownload("./sprites/grass4.png");
+ASSET_MANAGER.queueDownload("./sprites/grass2.png");
+ASSET_MANAGER.queueDownload("./sprites/grass.png");
+ASSET_MANAGER.queueDownload("./sprites/bush.png");
+ASSET_MANAGER.queueDownload("./sprites/stone.png");
+ASSET_MANAGER.queueDownload("./sprites/boxes.png");
+ASSET_MANAGER.queueDownload("./sprites/walls.jpg");
+
+ASSET_MANAGER.queueDownload("./sprites/coin.png");
+ASSET_MANAGER.queueDownload("./sprites/crystal.png");
+ASSET_MANAGER.queueDownload("./sprites/gem.png");
+ASSET_MANAGER.queueDownload("./sprites/lockeddoors.png");
+ASSET_MANAGER.queueDownload("./sprites/lock.png");
+ASSET_MANAGER.queueDownload("./sprites/key.png");
+
+ASSET_MANAGER.queueDownload("./sprites/firebreath.png");
+ASSET_MANAGER.queueDownload("./sprites/shadowball.png");
+ASSET_MANAGER.queueDownload("./sprites/arrow.png");
+ASSET_MANAGER.queueDownload("./sprites/lightning.png");
+ASSET_MANAGER.queueDownload("./sprites/fireball.png");
+ASSET_MANAGER.queueDownload("./sprites/slash.png");
+ASSET_MANAGER.queueDownload("./sprites/bonedart.png");
+ASSET_MANAGER.queueDownload("./sprites/scratch.png");
+ASSET_MANAGER.queueDownload("./sprites/sonicwave.png");
+ASSET_MANAGER.queueDownload("./sprites/sparkle.png");
+ASSET_MANAGER.queueDownload("./sprites/missile.png");
+ASSET_MANAGER.queueDownload("./sprites/laser.png");
+ASSET_MANAGER.queueDownload("./sprites/superslash.png");
+ASSET_MANAGER.queueDownload("./sprites/spiritball.png");
+ASSET_MANAGER.queueDownload("./sprites/spell.png");
+
+ASSET_MANAGER.queueDownload("./sprites/archer.png");
+ASSET_MANAGER.queueDownload("./sprites/mage.png");
+ASSET_MANAGER.queueDownload("./sprites/Hero.png");
+ASSET_MANAGER.queueDownload("./sprites/knightFullSpriteSheet.png");
+ASSET_MANAGER.queueDownload("./sprites/dragon.png");
+ASSET_MANAGER.queueDownload("./sprites/goblin.png");
+ASSET_MANAGER.queueDownload("./sprites/bat.png");
+ASSET_MANAGER.queueDownload("./sprites/skeleton.png");
+ASSET_MANAGER.queueDownload("./sprites/RPGW_Grassland_v1.3/MainLevBuild.png");
+ASSET_MANAGER.queueDownload("./sprites/RPGW_Grassland_v1.3/decorative.png");
+ASSET_MANAGER.queueDownload("./sprites/RPG_Buildings_CASTLE_v1.0/castle_interriors.png");
+
+//music here
+ASSET_MANAGER.queueDownload("./music/Short_Casual_Loop_2.mp3");
+ASSET_MANAGER.queueDownload("./music/Lyric__Fantasy_Theme_2.mp3");
+ASSET_MANAGER.queueDownload("./music/Lyric__Fantasy_Theme_1.mp3");
+
+
+ASSET_MANAGER.downloadAll(function () {
+	var gameEngine = new GameEngine();
+
+	ASSET_MANAGER.autoRepeat("./music/Short_Casual_Loop_2.mp3");
+	ASSET_MANAGER.autoRepeat("./music/Lyric__Fantasy_Theme_2.mp3");
+	ASSET_MANAGER.autoRepeat("./music/Lyric__Fantasy_Theme_1.mp3");
+
+	PARAMS.BLOCKWIDTH = PARAMS.BITWIDTH * PARAMS.SCALE;
+
+	var canvas = document.getElementById('gameWorld');
+	var ctx = canvas.getContext('2d');
+
+	PARAMS.CANVAS_WIDTH = canvas.width;
+	PARAMS.CANVAS_HEIGHT = canvas.height;
+	gaameEngine.init(ctx);
+	
+	//keeps scaled sprites from blurring, courtesy of Espen Storfjell
+	ctx.imageSmoothingEnabled = false;
+
+	gameEngine.init(ctx);
+	new SceneManager(gameEngine);
+
+	//console.log(gameEngine.entities.length);
+
+	gameEngine.start();
+});
---
+++ sceneManager.js
@@ -0,0 +1,371 @@
+class SceneManager {
+    constructor(game) {
+        this.game = game;
+        this.game.camera = this;
+        this.x = 0;
+        this.y = 0;
+        this.midpoint = PARAMS.CANVASWIDTH/2;
+        this.padding = 5;
+
+        // uncomment this when starting on startMap
+		// this.heroX = 1024;
+        // this.heroY = 1900;
+        this.heroX = 130;
+        this.heroY = 130;
+
+        this.coins = 0;
+        this.crystals = 0;
+        this.crystalAttackPower = 50;
+        this.crystalDefensePower = 50;
+        this.attackUpgradeCost = 1;
+        this.defenseUpgradeCost = 2;
+        this.healthUpgradeCost = 3;
+        this.attackUpgrade = 2;
+        this.defenseUpgrade = 1;
+        this.healthUpgrade = 10;
+        this.battle = false;
+
+        this.hero = new Hero(this.game, this.heroX, this.heroY);
+        this.cleric = new Cleric(this.game, this.heroX, this.heroY+30);
+		this.archer = new Archer(this.game, this.heroX, this.heroY+60);
+        this.mage = new Mage(this.game, this.heroX, this.heroY+90);
+        
+        // commented this out because the dragon should be added as JSON
+        // otherwise he shows up in every map.  I'm pretty sure every entity
+        // will behave this way, so it's important to add them via JSON from
+        // now on.
+        // this.boss = new Dragon(this.game, 350, 200);
+
+        buildMapData();
+        
+        this.loadTitleScreen(this.game, 0, 0);
+    }
+
+    draw(ctx) {
+        // Do nothing
+    }
+
+    updateAudio() {
+        var mute = document.getElementById("mute").checked;
+        var volume = document.getElementById("volume").value;
+
+        ASSET_MANAGER.muteAudio(mute);
+        ASSET_MANAGER.adjustVolume(volume);
+    };
+    
+    update() {
+        if (this.hero.stats[0] <= 0) { // hero dies
+            this.hero.reset();
+            this.reset();
+            this.loadTitleScreen(this.game, 0, 0);
+        }
+        PARAMS.DEBUG = document.getElementById("debug").checked;
+        switch (this.game.currentState) {
+            case this.game.gameStates[0]:
+                if (this.game.attack1) this.loadMap(this.game.currentMap, this.heroX, this.heroY);
+                break;
+            case this.game.gameStates[1]:
+                // center camera on hero during level exploration
+                this.x = this.hero.x - this.midpoint + this.hero.width/2 * PARAMS.SCALE * this.hero.scale;
+                this.y = this.hero.y - PARAMS.CANVASHEIGHT/2 + this.hero.height/2 * PARAMS.SCALE * this.hero.scale;
+                
+                // press N to switch to a battle scene
+                if(this.game.n){
+                    this.heroX = this.hero.x;
+                    this.heroY = this.hero.y;
+                    this.game.currentState = this.game.gameStates[2];
+                    this.sleep(200);
+                    this.battle = true;
+                    this.loadBattle();
+                }
+                break;
+            case this.game.gameStates[2]:
+                // center camera in middle of battle
+                this.x = 0;
+                this.y = 0;
+                
+                // press M to go back to the current dungeon map
+                if(this.game.m){
+                    this.sleep(200);
+                    this.battle = false;
+                    this.loadMap(this.game.currentMap, this.heroX, this.heroY);
+                }
+                break;
+        }
+    };
+                
+    loadTitleScreen(game, x, y) {
+        this.game.currentState = this.game.gameStates[0];
+        this.game.entities = [];
+        this.game.addEntity(new TitleScreen(game, x, y));
+    }
+
+
+    loadlevel(level, x, y, transition, title) {
+        if (level.music && !this.title) {
+            ASSET_MANAGER.pauseBackgroundMusic();
+            ASSET_MANAGER.playAsset(level.music);
+        }
+    }
+
+    
+    loadMap(map, x, y) {
+        this.game.currentState = this.game.gameStates[1];
+        this.game.entities = [];
+        this.x = 0;
+        this.y = 0;
+        if (map.CastleFloor1) {
+            for (var i = 0; i < map.CastleFloor1.length; i++) {
+                let ent = map.CastleFloor1[i];
+                this.game.addEntity(new CastleFloor1(this.game, ent.x, ent.y));
+            }
+        }
+        if (map.CastleWall1Mid) {
+            for (var i = 0; i < map.CastleWall1Mid.length; i++) {
+                let ent = map.CastleWall1Mid[i];
+                this.game.addEntity(new CastleWall1Mid(this.game, ent.x, ent.y));
+            }
+        }
+        if (map.CastleWall1LeftCorner) {
+            for (var i = 0; i < map.CastleWall1LeftCorner.length; i++) {
+                let ent = map.CastleWall1LeftCorner[i];
+                this.game.addEntity(new CastleWall1LeftCorner(this.game, ent.x, ent.y));
+            }
+        }
+        if (map.CastleWall1RightCorner) {
+            for (var i = 0; i < map.CastleWall1RightCorner.length; i++) {
+                let ent = map.CastleWall1RightCorner[i];
+                this.game.addEntity(new CastleWall1RightCorner(this.game, ent.x, ent.y));
+            }
+        }
+        if (map.CastleWall1RightEdge) {
+            for (var i = 0; i < map.CastleWall1RightEdge.length; i++) {
+                let ent = map.CastleWall1RightEdge[i];
+                this.game.addEntity(new CastleWall1RightEdge(this.game, ent.x, ent.y));
+            }
+        }
+        if (map.Grass1) {
+            for (var i = 0; i < map.Grass1.length; i++) {
+                let ent = map.Grass1[i];
+                this.game.addEntity(new Grass1(this.game, ent.x, ent.y))
+            }
+        }
+        if (map.StonePath) {
+            for (var i = 0; i < map.StonePath.length; i++) {
+                let ent = map.StonePath[i];
+                this.game.addEntity(new StonePath(this.game, ent.x, ent.y))
+            }
+        }
+        if (map.DirthPath) {
+            for (var i = 0; i < map.DirtPath.length; i++) {
+                let ent = map.DirtPath[i];
+                this.game.addEntity(new DirtPath(this.game, ent.x, ent.y));
+            }
+        }
+        if (map.Bush) {
+            for (var i = 0; i < map.Bush.length; i++) {
+                let ent = map.Bush[i];
+                this.game.addEntity(new Bush(this.game, ent.x, ent.y));
+            }
+        }
+        if (map.Rock) {
+            for (var i = 0; i < map.Rock.length; i++) {
+                let ent = map.Rock[i];
+                this.game.addEntity(new Rock(this.game, ent.x, ent.y));
+            }
+        }
+        if (map.Box) {
+            for (var i = 0; i < map.Box.length; i++) {
+                let ent = map.Box[i];
+                this.game.addEntity(new Box(this.game, ent.x, ent.y, ent.item));
+            }
+        }
+        if (map.Tree) {
+            for (var i = 0; i < map.Tree.length; i++) {
+                let ent = map.Tree[i];
+                this.game.addEntity(new Tree(this.game, ent.x, ent.y));
+            }
+        }
+        if (map.Wall) {
+            for (var i = 0; i < map.Wall.length; i++) {
+                let ent = map.Wall[i];
+                this.game.addEntity(new Wall(this.game, ent.x, ent.y));
+            }
+        }
+        if (map.Water) {
+            for (var i = 0; i < map.Water.length; i++) {
+                let ent = map.Water[i];
+                this.game.addEntity(new Water(this.game, ent.x, ent.y));
+            }
+        }
+        if (map.Lock) {
+            for (var i = 0; i < map.Lock.length; i++) {
+                let ent = map.Lock[i];
+                this.game.addEntity(new Lock(this.game, ent.x, ent.y));
+            }
+        }
+        if (map.Coin) {
+            for (var i = 0; i < map.Coin.length; i++) {
+                let ent = map.Coin[i];
+                this.game.addEntity(new Coin(this.game, ent.x, ent.y));
+            }
+        }
+        if (map.Crystal) {
+            for (var i = 0; i < map.Crystal.length; i++) {
+                let ent = map.Crystal[i];
+                this.game.addEntity(new Crystal(this.game, ent.x, ent.y));
+            }
+        }
+        if (map.Key) {
+            for (var i = 0; i < map.Key.length; i++) {
+                let ent = map.Key[i];
+                this.game.addEntity(new Key(this.game, ent.x, ent.y));
+            }
+        }
+        if (map.Goblin) {
+            for (var i = 0; i < map.Goblin.length; i++) {
+                let ent = map.Goblin[i];
+                this.game.addEntity(new Goblin(this.game, ent.x, ent.y));
+            }
+        }
+        if (map.Bat) {
+            for (var i = 0; i < map.Bat.length; i++) {
+                let ent = map.Bat[i];
+                let bat = new Bat(this.game, ent.x, ent.y)
+                this.game.addEntity(bat);
+                this.game.addEntity(new Scratch(this.game, ent.x, ent.y, bat));
+            }
+        }
+        if (map.Skeleton) {
+            for (var i = 0; i < map.Skeleton.length; i++) {
+                let ent = map.Skeleton[i];
+                let skeleton = new Skeleton(this.game, ent.x, ent.y)
+                this.game.addEntity(skeleton);
+                this.game.addEntity(new DeathStare(this.game, ent.x, ent.y, skeleton));
+            }
+        }
+        if (map.Dragon) {
+            for (var i = 0; i < map.Dragon.length; i++) {
+                let ent = map.Dragon[i];
+                let theDragon = new Dragon(this.game, ent.x, ent.y);
+                this.game.addEntity(theDragon);
+                this.game.addEntity(new FireBreath(this.game, ent.x, ent.y, theDragon));
+            }
+        }
+        // this.game.addEntity(this.boss);
+        // this.game.addEntity(new FireBreath(this.game, this.boss.x, this.boss.y));
+        //this.game.addEntity(new ShadowBall(this.game, this.boss.x, this.boss.y, this.boss.facing));
+
+        if (map.Archer) {
+            for (var i = 0; i < map.Archer.length; i++) {
+                let ent = map.Archer[i];
+                this.game.addEntity(new Archer(this.game, ent.x, ent.y));
+            }
+        }
+        if (map.Mage) {
+            for (var i = 0; i < map.Mage.length; i++) {
+                let ent = map.Mage[i];
+                let mage = new Mage(this.game, ent.x, ent.y);
+                this.game.addEntity(mage);
+                this.game.addEntity(new Lightning(this.game, ent.x, ent.y, mage));
+            }
+        }
+        if (map.Cleric) {
+            for (var i = 0; i < map.Cleric.length; i++) {
+                let ent = map.Cleric[i];
+                let cleric = new Cleric(this.game, ent.x, ent.y);
+                this.game.addEntity(cleric);
+                this.game.addEntity(new Spell(this.game, ent.x, ent.y, cleric));
+            }
+        }
+
+        //this.game.addEntity(this.cleric);
+        //this.game.addEntity(new Spell(this.game, this.cleric.x, this.cleric.y, this.cleric));
+
+        this.hero.x = x;
+        this.hero.y = y;
+        this.hero.battle = false;
+        this.game.addEntity(this.hero);
+        this.game.addEntity(new Slash(this.game, this.hero.x, this.hero.y, this.hero));
+        
+        this.game.addEntity(new HeadsUpDisplay(this.game));
+        this.game.addEntity(new MainMenu(this.game));
+        this.game.addEntity(new Shop(this.game));
+        this.game.addEntity(new Instructions(this.game));
+
+    }
+    
+    loadBattle() {
+        this.game.currentState = this.game.gameStates[2];
+        this.x = 0;
+        this.game.entities = [];
+		
+        // add decorations, etc. here
+        var k, l;
+        for (k = 0; k < 15; k++) {
+            for (l = 0; l < 15; l++) {
+                this.game.addEntity(new Grass1(this.game, k * 32, l * 32));
+            }
+        }
+		
+		// add player characters
+		this.game.addEntity(this.hero);
+		this.hero.x = 200;
+		this.hero.y = 5;
+		this.hero.battle = true;
+        this.game.addEntity(new Slash(this.game, this.hero.x, this.hero.y, this.hero));
+		//console.log("Battle hero: " + this.hero.x + ", " + this.hero.y);
+		this.game.addEntity(this.cleric);
+		this.cleric.x = this.hero.x;
+		this.cleric.y = this.hero.y + 35;
+        this.game.addEntity(new Spell(this.game, this.cleric.x, this.cleric.y, this.cleric));
+		//console.log("Battle cleric: " + this.cleric.x + ", " + this.cleric.y);
+		this.game.addEntity(this.archer);
+		this.archer.x = this.hero.x;
+		this.archer.y = this.cleric.y + 30;
+		this.archer.battle = true;
+		this.game.addEntity(this.mage);
+		this.mage.x = this.hero.x;
+		this.mage.y = this.archer.y + 40;
+		this.mage.battle = true;
+        this.game.addEntity(new Lightning(this.game, this.mage.x, this.mage.y, this.mage));
+		
+		// add enemies
+		this.goblin = new Goblin(this.game, 10, 10)
+		this.game.addEntity(this.goblin);
+		this.bat = new Bat(this.game, 20, 60)
+        this.game.addEntity(this.bat);
+        this.game.addEntity(new Scratch(this.game, this.bat.x, this.bat.y, this.bat));
+        
+		this.skeleton = new Skeleton(this.game, 20, 100)
+        this.game.addEntity(this.skeleton);
+        this.game.addEntity(new DeathStare(this.game, this.skeleton.x, this.skeleton.y, this.skeleton));
+        
+        this.game.addEntity(new HeadsUpDisplay(this.game));
+        this.game.addEntity(new MainMenu(this.game));
+        this.game.addEntity(new Shop(this.game));
+        this.game.addEntity(new Instructions(this.game));
+		
+		// load battle manager
+		this.battleManager = new BattleManager(this.game,[this.goblin,this.bat,this.skeleton],
+			[this.hero, this.cleric, this.archer, this.mage]);
+		this.game.addEntity(new BattleUI(this.game, [this.goblin,this.bat,this.skeleton],
+			[this.hero, this.cleric, this.archer, this.mage]));
+    }
+    
+    sleep(milliseconds) {
+        const date = Date.now();
+        let currentDate = null;
+        do {
+            currentDate = Date.now();
+        } while (currentDate - date < milliseconds);
+    };
+    
+    reset() {
+        this.coins = 0;
+        this.crystals = 0;
+        this.game.currentState = this.game.gameStates[0];
+        this.game.currentMap = this.game.gameMaps[0];
+    }
+	
+}
